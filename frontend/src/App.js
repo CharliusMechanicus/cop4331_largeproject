e18a8514f7a7e51e522dc1499812059d4a12f754
TangyrenMidKing <65498450+TangyrenMidKing@users.noreply.github.com>
2021-07-05 21:50:56 -0400

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,48 @@
-// THIS IS THE ENTRY POINT TO OUR APP
-// THE ONLY THING THAT SHOULD OCCUR HERE IS DIRECTING PEOPLE TO THE CORRECT PAGE OR RESOURCE
-
-/***********
-|  IMPORTS |
-************/
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom";
 
-/***********************************************************************************************************/
+import MainPage from './Pages/MainPage'
+import SignUp from './Pages/SignUp'
+import LogIn from './Pages/LogIn'
+import ResetPassword from './Pages/ResetPassword';
+import Cards from './Pages/Card';
+import MatchList from './Pages/Match';
 
 function App() {
 
-  /* NOTICE THE <Switch> TAGS. THEY ACT LIKE SWITCH STATEMENTS IN C, BUT YOU DON'T HAVE TO 'break'
-     AFTER A MATCHING 'case'. <Route> TAGS ARE LIKE THE CASES. <Redirect> ACTS LIKE THE 'default' CASE */
-
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact>
-          // INSERT HOME INDEX PAGE THAT YOU IMPORTED HERE
-        </Route>
-        <Redirect to="/" />
-      </Switch>  
-    </Router>
+  <Router >
+    <Switch>
+      
+      <Route path="/" exact>
+        <MainPage />
+      </Route>
+
+      <Route path="/signup" exact>
+        <SignUp />
+      </Route>
+
+      <Route path="/login" exact>
+        <LogIn />
+      </Route>
+
+      <Route path="/resetpassword" exact>
+        <ResetPassword />
+      </Route>
+
+      <Route path="/card" exact>
+        <Cards />
+      </Route>
+
+      <Route path="/card/match" exact>
+        <MatchList />
+      </Route>
+
+      <Redirect to="/" />
+    </Switch>  
+  </Router>
   );
-
 }
-
-/***********************************************************************************************************/
 
 export default App;
