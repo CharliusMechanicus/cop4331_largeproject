9faea2e1277e899facff0897f7fc3acc5d592fc8
Charlie Tan <charlie.tan@knights.ucf.edu>
2021-07-05 17:49:44 -0700

    fixed property names for JSON output in register API endpoint. also finished first draft of login API endpoint.

--- charlie_api.js
+++ charlie_api.js
@@ -98,12 +98,113 @@ exports.setApp = function(app, client)
 
     /*********************************************************************************************/
 
-    json_response_obj = {registration_success_bool, user_email_str, user_isgroup_bool};
+    json_response_obj = {success_bool : registration_success_bool, email_str : user_email_str,
+      is_group_bool : user_isgroup_bool};
+      
     res.status(200).json(json_response_obj);
     
   }); // END REGISTER API ENDPOINT
 
-  /***********************************************************************************************/
+  /********************************** NEXT API ENDPOINT ******************************************/
+
+  // LOGIN API ENDPOINT
+  // INPUT: JSON OBJECT (email_str, password_str)
+  // OUTPUT: JSON OBJECT (success_bool, email_str, is_group_bool, access_token_str)
+  app.post('/api/login', async (req, res, next) =>
+  {
+
+    /********************
+    |  LOCAL VARIABLES  |
+    *********************/
+    let request_body_data;
+    let user_email_str;
+    let user_password_str;
+    let user_isgroup_bool;
+    let collection_str;
+    
+    let database;
+    let database_results_array;
+    let access_token_str;
+
+    let login_success_bool;
+    let json_response_obj;
+    
+    let my_token_functions = require("./createJWT.js");
+
+    /*********************************************************************************************/
+
+    // EXTRACT INFORMATION
+    request_body_data = req.body;
+    user_email_str = request_body_data.email_str;
+    user_password_str = request_body_data.password_str;
+
+    /*********************************************************************************************/
+
+    // CONNECT TO DATABASE
+    try
+    {
+      database = client.db();
+    }
+    catch(error)
+    {
+      console.log(error.message);
+    }
+
+    collection_str = user_exists_in_this_collection(user_email_str, database);
+
+    /*********************************************************************************************/
+
+    // IF USER COULD NOT BE FOUND IN DATABASE
+    if(!collection_str)
+    {
+      login_success_bool = false;
+      user_isgroup_bool = undefined;
+      access_token_str = "";
+    }
+    
+    // OTHERWISE, USER WAS FOUND IN DATABASE
+    else
+    {
+      // CONTINUE WITH PASSWORD VERIFICATION
+      try
+      {
+        database_results_array =
+          await database.collection(collection_str).find(
+          {email : user_email_str, password : user_password_str} ).toArray();
+        
+        // IF PASSWORD WAS VERIFIED
+        if(database_results_array.length > 0)
+        {
+          login_success_bool = true;
+          user_isgroup_bool = is_this_collection_a_group(collection_str);
+          access_token_str = my_token_functions.createToken(user_email_str);
+        }
+        
+        // OTHERWISE, PASSWORD IS INCORRECT
+        else
+        {
+          login_success_bool = false;
+          user_isgroup_bool = undefined;
+          access_token_str = "";
+        }
+      }
+      
+      catch(error)
+      {
+        console.log(error);
+      }
+    }
+    
+    /*********************************************************************************************/
+    
+    json_response_obj = {success_bool : login_success_bool, email_str : user_email_str,
+      is_group_bool : user_isgroup_bool, access_token_str : access_token_str};
+    
+    res.status(200).json(json_response_obj);
+    
+  }); // END LOGIN API ENDPOINT
+
+  /*********************************** END API ENDPOINTS *****************************************/
 
   /***************************
   |  USER-DEFINED FUNCTIONS  |
@@ -149,6 +250,71 @@ exports.setApp = function(app, client)
     }
     
     return group_obj;
+  }
+
+  /************************************* NEXT FUNCTION *******************************************/
+
+  // 'database' IS ASSUMED TO BE A VALID CONNECTION
+  // RETURNS STRING "individuals" OR "groups" FOR THE COLLECTION 'user_email_str' IS FOUND IN
+  // RETURNS EMPTY STRING ("") IF USER COULD NOT BE FOUND IN ANY COLLECTION
+  // RETURNS 'undefined' IF ERROR OCCURS WHILE PERFORMING DATABASE OPERATIONS
+  async function user_exists_in_this_collection(user_email_str, database)
+  {
+    let database_results_array;
+    let collection_str;
+
+    try
+    {
+      database_results_array =
+        await database.collection("individuals").find( {email : user_email_str} ).toArray();
+
+      // IF USER IS NOT FOUND IN THE 'individuals' COLLECTION
+      if(database_results_array.length === 0)
+      {
+        // KEEP SEARCHING FOR USER IN 'groups' COLLECTION
+        database_results_array =
+          await database.collection("groups").find( {email : user_email_str} ).toArray();
+      
+        // IF USER IS NOT FOUND IN 'groups' COLLECTION
+        if(database_results_array.length === 0)
+        {
+          collection_str = "";
+        }
+      
+        // OTHERWISE, USER IS FOUND IN THE 'groups' COLLECTION
+        else
+        {
+          collection_str = "groups";
+        }
+      }
+    
+      // OTHERWISE, USER IS FOUND IN THE 'individuals' COLLECTION
+      else
+      {
+        collection_str = "individuals";
+      }
+    } // END TRY
+    
+    catch(error)
+    {
+      console.log(error);
+    }
+    
+    return collection_str;
+  }
+
+  /************************************* NEXT FUNCTION *******************************************/
+
+  // RETURNS 'true' IF 'collection_str' REPRESENTS THE 'groups' COLLECTION, 'false' OTHERWISE.
+  // NOTE THAT A 'false' RETURN DOES NOT AUTOMATICALLY MEAN THAT 'collection_str' REPRESENTS THE..
+  // ..'individuals' COLLECTION
+  function is_this_collection_a_group(collection_str)
+  {
+    if(collection_str === "groups")
+      return true;
+
+    else
+      return false;
   }
 
 }; // END setApp
