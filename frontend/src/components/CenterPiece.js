bd4e0e3af34d235f74a2ef55970d0c1bfd539cc7
rna-devknight <ricardo.nunesalcobia@knights.ucf.edu>
2021-07-13 21:37:32 -0400

    Components examples

---
+++ frontend/src/components/CenterPiece.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+
+function CenterPiece()
+{
+    return(
+        <div className="center-piece">
+            <div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default CenterPiece;
