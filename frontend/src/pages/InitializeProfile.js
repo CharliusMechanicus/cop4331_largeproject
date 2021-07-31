f78b532c2c091088e969b36d890ed52123e8b30a
rna-devknight <ricardo.nunesalcobia@knights.ucf.edu>
2021-07-30 23:06:06 -0400

    Initial Initialization Conversion

--- frontend/src/pages/InitializeProfile.js
+++ frontend/src/pages/InitializeProfile.js
@@ -6,7 +6,7 @@ export default function InitializeProfile()
     const api_path = 'https://kindling-lp.herokuapp.com/';
     const[message, setMessage] = useState('');
     
-    const[showDescribetion, setShowDescribetion] = useState(false);
+    const[showDescription, showDescription] = useState(false);
     const[game_development, set_game_development] = useState(false);
     const[app_development, set_app_development] = useState(false);
     const[web_development, set_web_development] = useState(false);
@@ -19,7 +19,7 @@ export default function InitializeProfile()
     const[lab_partners, set_lab_partners] = useState(false);
     const[research, set_research] = useState(false);
     const[other, set_other] = useState(false);
-    
+
     var description;
     var token = JSON.parse(localStorage.getItem('user_data'));
     
@@ -134,33 +134,75 @@ export default function InitializeProfile()
     }
 
     return (
-        <div class='continue_signup'>
-            { showDescribetion ? 
-                <div id='signup_description'>
-                    <h1>Add some describetion</h1><br/>
-                    <input type="text" className="description" placeholder='description' ref={(c) => description = c}></input><br/>
-                    <span>{message}</span><br/>
-                    <button className='btn' onClick={save_profile}>Save</button>
-                </div>
+        <Container>
+            {showDescription ?
+                <Row>
+                    <Col>
+                        <Row>
+                            <Col>
+                                <h1>Add some descriptions</h1>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <input type="text" className="description" placeholder='description' ref={(c) => description = c}></input>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <button className='btn' onClick={save_profile}>Save</button>
+                            <span>{message}</span>
+                        </Row>
+                    </Col>
+                </Row>
                 :
-                <div class='signup_tags'>
-                    <h1>What are your interests</h1>
-                    <input type='button' className='select_btn' onClick={() => set_game_development(!game_development)} value='game development'/>
-                    <input type='button' className='select_btn' onClick={() => set_app_development(!app_development)}  value='app development'/><br/>
-                    <input type='button' className='select_btn' onClick={() => set_web_development(!web_development)}  value='web development'/>
-                    <input type='button' className='select_btn' onClick={() => set_robotics(!robotics)}  value='robotics'/><br/>
-                    <input type='button' className='select_btn' onClick={() => set_graphic_design(!graphic_design)}  value='graphic design'/>
-                    <input type='button' className='select_btn' onClick={() => set_writer(!writer)}  value='writer'/><br/>
-                    <input type='button' className='select_btn' onClick={() => set_marketing(!marketing)}  value='marketing'/>
-                    <input type='button' className='select_btn' onClick={() => set_networking(!networking)}  value='networking'/><br/>
-                    <input type='button' className='select_btn' onClick={() => set_construction(!construction)}  value='construction'/>
-                    <input type='button' className='select_btn' onClick={() => set_lab_partners(!lab_partners)}  value='lab partners'/><br/>
-                    <input type='button' className='select_btn' onClick={() => set_research(!research)}  value='research'/>
-                    <input type='button' className='select_btn' onClick={() => set_other(!other)}  value='other'/><br/>
-                    <span>{message}</span><br/>
-                    <button class='btn' onClick={save_tages}>Next</button><br/>
-                </div>
+                <Row>
+                    <Col>
+                        <Row>
+                            <Col>
+                                <h1>What are your interests</h1>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <input type='button' className='select_btn' onClick={() => set_game_development(!game_development)} value='game development'/>
+                            
+                                <input type='button' className='select_btn' onClick={() => set_app_development(!app_development)}  value='app development'/>
+                            </Col>
+                            <Col>
+                                <input type='button' className='select_btn' onClick={() => set_web_development(!web_development)}  value='web development'/>
+                            
+                                <input type='button' className='select_btn' onClick={() => set_robotics(!robotics)}  value='robotics'/>
+                            </Col>
+                            <Col>
+                                <input type='button' className='select_btn' onClick={() => set_graphic_design(!graphic_design)}  value='graphic design'/>
+            
+                                <input type='button' className='select_btn' onClick={() => set_writer(!writer)}  value='writer'/>
+                            </Col>
+                            <Col>
+                                <input type='button' className='select_btn' onClick={() => set_marketing(!marketing)}  value='marketing'/>
+                    
+                                <input type='button' className='select_btn' onClick={() => set_networking(!networking)}  value='networking'/>
+                            </Col>
+                            <Col>
+                                <input type='button' className='select_btn' onClick={() => set_construction(!construction)}  value='construction'/>
+
+                                <input type='button' className='select_btn' onClick={() => set_lab_partners(!lab_partners)}  value='lab partners'/>
+                            </Col>
+                            <Col>
+                                <input type='button' className='select_btn' onClick={() => set_research(!research)}  value='research'/>
+                                
+                                <input type='button' className='select_btn' onClick={() => set_other(!other)}  value='other'/>
+                            </Col>
+                            <Col>
+                                <span>{message}</span>
+
+                                <button class='btn' onClick={save_tages}>Next</button>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+
             }
-        </div>
+        </Container>
     );
 }
